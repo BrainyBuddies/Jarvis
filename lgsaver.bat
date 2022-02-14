a718ce27a94cbac59d9843c4f04f048dae0298ca
BrainyBuddies <99661109+BrainyBuddies@users.noreply.github.com>
2022-02-14 22:48:27 +0530

    Add files via upload

--- lgsaver.bat
+++ lgsaver.bat
@@ -1,2 +1,2 @@
-echo User Last Checked For Updates At: occurrence date: %date% occurrence time(24hrs format): %time%.> .\Update_Logs\UPDATE-INF.log
+echo User Last Checked For Updates At: occurrence date: %date% occurrence time(24hrs format): %time%.> .\Data\UPDATE-INF.log
 exit
